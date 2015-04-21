1f2b6dffc9fb70565a2c64156082ec9d167bcb3b
ssc9309 <ssc9309@hotmail.com>
2015-04-21 16:25:00 -0400

    Initial Commit
    Having trouble with displaying the LifeGrid.
    Too Slow.

---
+++ GameOfLife/Main.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GameOfLife
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Randomize = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_TextLife = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(12, 12);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            // 
+            // btn_Randomize
+            // 
+            this.btn_Randomize.Location = new System.Drawing.Point(94, 12);
+            this.btn_Randomize.Name = "btn_Randomize";
+            this.btn_Randomize.Size = new System.Drawing.Size(75, 23);
+            this.btn_Randomize.TabIndex = 1;
+            this.btn_Randomize.Text = "Randomize";
+            this.btn_Randomize.UseVisualStyleBackColor = true;
+            this.btn_Randomize.Click += new System.EventHandler(this.btn_Randomize_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 800);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_TextLife
+            // 
+            this.lbl_TextLife.AutoEllipsis = true;
+            this.lbl_TextLife.AutoSize = true;
+            this.lbl_TextLife.Location = new System.Drawing.Point(13, 42);
+            this.lbl_TextLife.Name = "lbl_TextLife";
+            this.lbl_TextLife.Size = new System.Drawing.Size(45, 13);
+            this.lbl_TextLife.TabIndex = 4;
+            this.lbl_TextLife.Text = "TextLife";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 961);
+            this.Controls.Add(this.lbl_TextLife);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Randomize);
+            this.Controls.Add(this.btn_Start);
+            this.Name = "Main";
+            this.Text = "Game Of Life";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Randomize;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_TextLife;
+
+    }
+}
+
---
+++ GameOfLife/Main.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Main : Form
+    {
+        /* 0,0  1,0  2,0  3,0  
+         * 0,1  1,1
+         * 0,2
+         * 0,3
+         * 0,4            4,4
+         */
+
+        //private int gridLengthX = 1000;
+        //private int gridLengthY = 800;
+
+        private int gridLengthX = 200;
+        private int gridLengthY = 200;
+
+        //array of array
+        private int[][] LifeGrid;
+
+        private Graphics drawingArea;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //LifeGrid initialization
+
+            LifeGrid = new int [gridLengthX][];
+
+            for (int x = 0; x < gridLengthX; x++)
+            {
+                LifeGrid[x] = new int[gridLengthY];
+                for (int y = 0; y<gridLengthY; y++)
+                {
+                    LifeGrid[x][y] = 0;
+                }
+            }
+
+
+            //graphics initialization
+            drawingArea = pictureBox1.CreateGraphics();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Graphics graphicObject = Main.create
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            int x = 900;
+            int y = 50;
+            int width = 100;
+            int height = 50;
+
+            drawingArea.DrawRectangle(blackPen, x, y, width, height);
+        }
+
+        private void Main_Paint(object sender, PaintEventArgs e)
+        {
+
+            Pen blackPen = new Pen(Color.Black, 3);
+            Brush redBrush = new SolidBrush(Color.Red);
+            int x = 200;
+            int y = 50;
+            int width = 50;
+            int height = 50;
+
+            //e.Graphics.DrawRectangle(blackPen, x, y, width, height);
+            e.Graphics.FillRectangle(redBrush, x, y, width, height);
+        }
+
+        private void btn_Randomize_Click(object sender, EventArgs e)
+        {
+            //there are 1000x800 plots on the grid
+            //start with 1000?
+
+            int startLifeCount = 1000;
+            Random random = new Random();
+
+            while (startLifeCount > 0)
+            {
+                int x = random.Next(0, gridLengthX - 1);
+                int y = random.Next(0, gridLengthY - 1);
+
+                if (LifeGrid[x][y] == 0)
+                {
+                    LifeGrid[x][y] = 1;
+                    startLifeCount--;
+                }
+            }
+
+            //DrawLife();
+            //DrawTextLife();
+        }
+
+        private void DrawLife()
+        {
+            Brush greenBrush = new SolidBrush(Color.Green);
+            Brush blackBrush = new SolidBrush(Color.Black);
+
+            for (int x = 0; x < gridLengthX; x++)
+            {
+                for (int y = 0; y < gridLengthY; y++)
+                {
+                    if (LifeGrid[x][y] == 1)
+                    {
+                        drawingArea.FillRectangle(greenBrush, x, y, 1, 1);
+                    }
+                    else
+                    {
+                        drawingArea.FillRectangle(blackBrush, x, y, 1, 1);
+                    }
+                }
+            }
+        }
+
+        private void DrawTextLife()
+        {
+            //StringBuilder textLife = new StringBuilder();
+            //string row = "";
+
+            lbl_TextLife.Text = "";
+
+            for (int y = 0; y < gridLengthY; y ++)
+            {
+                for (int x = 0; x < gridLengthX; x ++)
+                {
+                    if (LifeGrid[x][y] == 1)
+                    {
+                        //drawingArea.FillRectangle(greenBrush, x, y, 1, 1);
+
+                        //row += "O ";
+                        lbl_TextLife.Text += "O ";
+                    }
+                    else
+                    {
+                        //drawingArea.FillRectangle(blackBrush, x, y, 1, 1);
+                        //row += "X ";
+                        lbl_TextLife.Text += "X ";
+                    }
+                }
+                //textLife += "\n";
+                //textLife.AppendLine(row);
+                lbl_TextLife.Text += "\n";
+            }
+
+            //lbl_TextLife.Text = textLife.ToString();
+        }
+    }
+}
